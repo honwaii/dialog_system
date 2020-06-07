84e4869d3998e9e1f41b59aa647b19a6863415b3
honwaii <honwaii@126.com>
2020-06-08 07:45:40 +0800

    update..

--- app/service/data_handler.py
+++ app/service/data_handler.py
@@ -5,6 +5,16 @@
 # @Email   : honwaii@126.com
 # @File    : data_handler.py
 
+import pandas as pd
+from app.util.cfg_operator import config
+
 
 def load_data():
+    path = config.get_config('data_path')
+    data = pd.read_csv(path, encoding='utf-8')
+    head = data.head(2)
+    print(head)
     return
+
+
+load_data()
