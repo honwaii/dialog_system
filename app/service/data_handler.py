3b0d99053e623efd7d105f2ee8756be7bb788540
honwaii <honwaii@126.com>
2020-06-10 00:39:22 +0800

    update..

--- app/service/data_handler.py
+++ app/service/data_handler.py
@@ -4,6 +4,7 @@
 # @Author  : honwaii
 # @Email   : honwaii@126.com
 # @File    : data_handler.py
+from collections import defaultdict
 
 import pandas as pd
 from app.util.cfg_operator import config
@@ -14,31 +15,66 @@ def load_data():
     path = config.get_config('data_path')
     data = pd.read_csv(path, encoding='utf-8')
     questions = data.get('question')
+    answer = data.get('answer')
     stop_words = load_stop_words()
-    results = []
+    word_dict = defaultdict()
     for each in range(len(questions)):
         try:
-            q = jieba.lcut(questions[each])
-            result = []
-            for e in q:
-                if e in stop_words:
-                    continue
-                result.append(e)
-            results.append(result)
+            generate_index_dict(questions[each], each, stop_words, word_dict)
+            # generate_index_dict(answer[each], each, stop_words, word_dict)
         except Exception:
             print(each)
-    print(results[0])
-    print(results[1])
-    return
+            print(questions[each])
+    return word_dict, stop_words, answer
 
 
 def load_stop_words():
     path = config.get_config('stop_word_path')
     with open(path, encoding='utf-8') as f:
         stop_words = f.readlines()
-        # print(len(stop_words))
+    for i in range(len(stop_words)):
+        stop_words[i] = stop_words[i].strip()
     return stop_words
 
 
-load_data()
-# load_stop_words()
+def generate_index_dict(doc, index, stop_words, word_dict):
+    item = jieba.lcut(doc)
+    for each in item:
+        if each in stop_words:
+            continue
+        temp = word_dict.get(each)
+        if temp is not None:
+            temp.append(index)
+            continue
+        word_dict[each] = [index]
+    return word_dict
+
+
+def search_word(sentence, stop_words, word_dict):
+    words = jieba.lcut(sentence)
+    temp = []
+    for each in words:
+        if each in stop_words:
+            continue
+        word_in_doc = word_dict.get(each)
+        if word_in_doc is None:
+            continue
+        temp.append(word_in_doc)
+    if len(temp) <= 0:
+        print('未在文档中找到匹配的问题。')
+        return
+    if len(temp) == 1:
+        return temp[0]
+    result = []
+    print(temp)
+    for i in range(len(temp) - 1):
+        if i == 0:
+            result = temp[i]
+        result = list(set(result).intersection(set(temp[i + 1])))
+        print(result)
+    return result
+
+
+word_dict, stop_words, answer = load_data()
+r = search_word('请问你找谁？', stop_words, word_dict)
+print(answer[r[0]])
